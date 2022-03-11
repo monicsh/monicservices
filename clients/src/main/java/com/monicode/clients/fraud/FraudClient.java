384f0817f94dece15de0b22dbb5f88bf93ec0b95
Monika Sharma <msharma@monmac.local>
2022-03-10 16:43:19 -0800

    added openfeign client

---
+++ clients/src/main/java/com/monicode/clients/fraud/FraudClient.java
@@ -0,0 +1,11 @@
+package com.monicode.clients.fraud;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient("fraud")
+public interface FraudClient {
+    @GetMapping(path = "api/v1/fraud-check/{customerID}")
+    public FraudCheckResponse isFraudster(@PathVariable("customerID") Integer customerID);
+}
