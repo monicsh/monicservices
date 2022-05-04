cf69c9cf2b22c652d5d2741ea60845544954e53d
Monika Sharma <msharma@monmac.local>
2022-05-04 11:14:37 -0700

    Setup for sending and receiving messages

---
+++ amqp1/src/main/java/com/monicode/ampq/RabbitMQConfigure.java
@@ -0,0 +1,58 @@
+package com.monicode.ampq;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@AllArgsConstructor
+public class RabbitMQConfigure {
+    // Ability to publish a message to queue
+
+    private final ConnectionFactory connectionFactory;
+
+    /*
+    Send message to Queue  //rabittemplate will allow to send messages
+     */
+    @Bean
+    public AmqpTemplate ampqTemplate(){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jacksonConverter());
+        return rabbitTemplate;
+    }
+
+    /*
+    Receive Messages from the Queue
+     */
+    @Bean
+    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory(){
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(jacksonConverter());
+        return factory;
+    }
+
+
+
+    @Bean
+    public MessageConverter jacksonConverter(){
+        MessageConverter jackson2JsonMessageConverter = new Jackson2JsonMessageConverter();
+        return jackson2JsonMessageConverter;
+    }
+}
+
+
+
+/*
+1. Setup for sending and receiving message
+2. Setup  Exchange and Queue
+3. Setup bind Exchange to Queue
+ */
