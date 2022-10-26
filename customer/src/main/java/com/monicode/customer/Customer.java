f51abd3bc448d20368d43beb830c661222ae854f
Monika Sharma <monic.sh@gmail.com>
2022-10-25 20:19:28 -0700

    added web later tests

--- customer/src/main/java/com/monicode/customer/Customer.java
+++ customer/src/main/java/com/monicode/customer/Customer.java
@@ -1,5 +1,8 @@
 package com.monicode.customer;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
--- customer/src/main/java/com/monicode/customer/CustomerRepository.java
+++ customer/src/main/java/com/monicode/customer/CustomerRepository.java
@@ -2,5 +2,9 @@ package com.monicode.customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    List<Customer> getByFirstName(String name);
 }
